0b0f6c409d4fcf549044d6a8576a2718a609f3c0
AndreCI <andre.cibils@gmail.com>
2018-03-21 16:10:38 +0100

    all fwd done

--- framework/modules/relu_layer.py
+++ framework/modules/relu_layer.py
@@ -5,7 +5,10 @@ import torch
 class ReLuLayer(Module):
 
     def forward(self, input):
-        return torch.relu(input)
+        #TODO: is this really the best way?
+        mask = input > 0 #Compute mask
+        mask = mask.type(torch.FloatTensor) #Change type of mask to allow mul
+        return torch.mul(input, mask) #mask the input
 
     def backward(self, *gradwrtoutput):
         pass
